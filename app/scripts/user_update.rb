5232d0b0f5428aff90e8d6743b103236effa6e61
kaustubhmo <kaustubhmo@cybage.com>
2021-10-04 17:07:32 +0530

    fix bug

--- app/scripts/user_update.rb
+++ app/scripts/user_update.rb
@@ -1,7 +1,13 @@
 class UserUpdate < KafkaBase
     def call
         $kafka_consumer.each_message(topic: "user_update") do |message|
-            puts message.offset, message.key, message.value
+            puts "----------------"
+            puts "Topic::#{message.topic}"
+            puts "partition::#{message.partition}"
+            puts "offset::#{message.offset}"
+            puts "key::#{message.key}"
+            puts "value::#{message.value}"
+            #puts message.offset, message.key, message.value
         end
     end
 end
